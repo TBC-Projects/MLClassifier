032d7d854292dc400a0ce15f895dba57da56180f
verbi3 <vaniabao34@gmail.com>
2026-01-12 20:42:20 -0800

    adding example ml model files

---
+++ doc/MLEngineering/VaniaInsightFaceExample/training.py
@@ -0,0 +1,6 @@
+#Load recognizer model and compare embeddings to threshold
+from backgroundprocessing import FaceRecognizer, monitor_folder
+
+recognizer = FaceRecognizer(ctx_id=0, threshold=0.6)
+recognizer.train("dataset")  # Or use recognizer.load("svm_model.pkl")
+recognizer.save("svm_face_model.pkl")
